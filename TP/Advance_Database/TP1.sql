17add1e55af63236e84ac4eb8d15f5d3f8a74eb4
PhengMenghour <phengmenghour@gmail.com>
2024-10-23 22:13:22 +0700

    SQL update

--- TP/Advance_Database/TP1.sql
+++ TP/Advance_Database/TP1.sql
@@ -8,6 +8,9 @@ describe products;
 
 select * from customers;
 select * from productlines;
+select * from products;
+select * from orders;
+select * from orderdetails;
 
 -- 1. Show first name and last name of customer from France.
 SELECT contactFirstName, contactLastName
@@ -39,9 +42,51 @@ SELECT
 	CASE
 		WHEN creditLimit > 80000 THEN "VIP"
         WHEN creditLimit < 80000 THEN "Normal"
-	END AS status
+	END AS customerStatus
 FROM customers;
 
 -- 6. Show all product which is in “classic car” product line.
+SELECT *
+FROM products
+WHERE productLine = "Classic cars";
+
+-- 7. Show all products containing “Toyota” in its name.
+SELECT *
+FROM products
+WHERE productName LIKE "%Toyota%";
+
+-- 8. Show all products with the scale smaller than 1:24.
+SELECT *
+FROM products
+WHERE CAST(SUBSTRING_INDEX(productScale, ':', -1) AS UNSIGNED) < 24;
+
+-- 9. Show all products from the vendor “Autoart Studio Design”.
+SELECT *
+FROM products
+WHERE productVendor = "Autoart Studio Design";
+
+-- 10. Show all products with MSRP (the manufacturer’s suggested retail price) higher than twice the cost of
+-- buyPrice.
+SELECT *
+FROM products
+WHERE MSRP > (buyPrice * 2);
+
+-- 11. Show information of sale representative of the customer “Land of toys Inc.”
+SELECT e.employeeNumber, e.firstName, e.lastName, e.extension, e.email, e.officeCode, e.jobTitle
+FROM customers c
+JOIN employees e ON c.salesRepEmployeeNumber = e.employeeNumber
+WHERE c.customerName = 'Land of Toys Inc.';
+
+-- 12. Show information about order that is shipped later than the required date.
+SELECT orderNumber, orderDate, requiredDate, shippedDate, status
+FROM orders
+WHERE shippedDate > requiredDate;
+
+-- 13. Show all orders made by “La Rochelle Gifts”.
+SELECT o.orderNumber, o.orderDate, o.requiredDate, o.shippedDate, o.status, o.comments
+FROM orders o
+JOIN customers c ON o.customerNumber = c.customerNumber
+WHERE c.customerName = 'La Rochelle Gifts';
+
 
 

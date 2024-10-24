2033a3914c2b6531261fa7d03e63f2d4fd68de8d
Pheng Menghour <menghour@Phengs-MacBook-Pro.local>
2024-10-24 16:01:08 +0700

    Done TP1 advance database

--- TP/Advance_Database/TP1.sql
+++ TP/Advance_Database/TP1.sql
@@ -3,14 +3,22 @@ use classicmodels;
 show tables;
 describe customers;
 describe employees;
+describe offices;
+describe orderdetails;
+describe orders;
+describe payments;
 describe productlines;
 describe products;
 
 select * from customers;
+select * from employees;
+select * from offices;
+select * from orderdetails;
+select * from orders;
+select * from payments;
 select * from productlines;
 select * from products;
-select * from orders;
-select * from orderdetails;
+
 
 -- 1. Show first name and last name of customer from France.
 SELECT contactFirstName, contactLastName
@@ -87,6 +95,164 @@ SELECT o.orderNumber, o.orderDate, o.requiredDate, o.shippedDate, o.status, o.co
 FROM orders o
 JOIN customers c ON o.customerNumber = c.customerNumber
 WHERE c.customerName = 'La Rochelle Gifts';
+
+-- 14. Show all products ordered by “La Rochelle Gifts”.
+SELECT p.productCode ,p.productName, od.quantityOrdered, od.priceEach
+FROM customers c
+JOIN orders o ON c.customerNumber = o.customerNumber
+JOIN orderdetails od ON o.orderNumber = od.orderNumber
+JOIN products p ON od.productCode = p.productCode
+WHERE c.customerName = "La Rochelle Gifts";
+
+-- 15. Show customer’s name who has ordered more than 15 products in once.
+SELECT c.customerName, o.orderNumber, SUM(od.quantityOrdered) AS totalProductsOrdered
+FROM customers c
+JOIN orders o ON c.customerNumber = o.customerNumber
+JOIN orderdetails od on o.orderNumber = od.orderNumber
+GROUP BY c.customerName, o.orderNumber
+HAVING SUM(od.quantityOrdered > 15);
+
+-- 16. Show the best-selling product in “2004-02”.
+SELECT p.productCode, p.productName, SUM(od.quantityOrdered) AS totalQuantity
+FROM orders o
+JOIN orderDetails od ON o.orderNumber = od.orderNumber
+JOIN products p ON od.productCode = p.productCode
+WHERE o.orderDate BETWEEN "2004-02-01" AND "2004-02-29"
+GROUP BY p.productCode, p.productName
+ORDER BY totalQuantity DESC
+LIMIT 1;
+
+
+-- 17. Find the lowest sale price of each product.
+SELECT p.productCode, p.productName, MIN(od.priceEach) AS lowestPrice
+FROM products p
+JOIN orderDetails od on p.productCode = od.productCode
+GROUP BY p.productCode, p.productName;
+
+-- 18. Find the biggest gape of sale price of each product.
+SELECT p.productCode, p.productName, MAX(od.priceEach)-MIN(od.priceEach) as priceGap
+FROM products p
+JOIN orderDetails od ON p.productCode = od.productCode
+GROUP BY p.productCode, p.productName
+ORDER BY priceGap DESC;
+
+-- 19. Find the product with the biggest gape of sale price.
+SELECT p.productCode, p.productName, MAX(od.priceEach)-MIN(od.priceEach) AS priceGap
+FROM products p
+JOIN orderDetails od ON p.productCode = od.productCode
+GROUP BY p.productCode, p.productName
+ORDER BY priceGap DESC
+LIMIT 1;
+
+-- 20. Find the total cost of all orders of each customer.
+SELECT c.customerName, SUM(od.quantityOrdered * od.priceEach) AS totalCost
+FROM customers c
+JOIN orders o ON c.customerNumber = o.customerNumber
+JOIN orderDetails od ON o.orderNumber = od.orderNumber
+GROUP BY c.customerName
+ORDER BY totalCost DESC;
+
+-- 21. Find the top customer (with the highest total sale cost) of each city.
+WITH CustomerTotal AS (
+	SELECT c.customerNumber, c.customerName, c.city, SUM(od.quantityOrdered * od.priceEach) AS totalCost
+    FROM customers c
+    JOIN orders o ON c.customerNumber =  o.customerNumber
+    JOIN orderDetails od ON o.orderNumber = od.orderNumber
+    GROUP BY c.customerNumber, c.customerName, c.city
+)
+
+SELECT city, customerName, totalCost
+FROM customerTotal ct1
+WHERE totalCost = (
+	SELECT MAX(ct2.totalCost)
+    FROM CustomerTotal ct2
+    WHERE ct2.city = ct1.city
+)
+ORDER BY city;
+
+-- 22. Show monthly income, purchase expense, and profit in a specific year. An order that is placed in which
+-- month would be considered as part of income of that month; the state of shipment and payment are taken into account.
+SELECT 
+  YEAR(o.orderDate) AS year,
+  MONTH(o.orderDate) AS month,
+    ROUND(SUM(pay.amount),2) AS income,
+    ROUND(SUM(od.quantityOrdered * od.priceEach),2) AS purchaseExpenses,
+	ROUND(SUM(pay.amount - (od.quantityOrdered * od.priceEach)),2) AS profit
+FROM orders o
+JOIN orderdetails od ON o.orderNumber = od.orderNumber
+JOIN products p ON od.productCode = p.productCode
+LEFT JOIN payments pay ON o.customerNumber = pay.customerNumber
+WHERE YEAR(o.orderDate) = 2004 AND o.STATUS IN ('Shipped', 'Completed')
+GROUP BY month(o.orderDate), year(o.orderDate)
+ORDER BY month(o.orderDate), year(o.orderDate);
+
+-- 23. The same as number 14, but we want months as columns and incomes, purcase_expense, and profit as rows.
+SELECT 
+    'Income' AS type,
+    SUM(CASE WHEN MONTH(o.orderDate) = 1 THEN od.quantityOrdered * od.priceEach ELSE 0 END) AS January,
+    SUM(CASE WHEN MONTH(o.orderDate) = 2 THEN od.quantityOrdered * od.priceEach ELSE 0 END) AS February,
+    SUM(CASE WHEN MONTH(o.orderDate) = 3 THEN od.quantityOrdered * od.priceEach ELSE 0 END) AS March,
+    SUM(CASE WHEN MONTH(o.orderDate) = 4 THEN od.quantityOrdered * od.priceEach ELSE 0 END) AS April,
+    SUM(CASE WHEN MONTH(o.orderDate) = 5 THEN od.quantityOrdered * od.priceEach ELSE 0 END) AS May,
+    SUM(CASE WHEN MONTH(o.orderDate) = 6 THEN od.quantityOrdered * od.priceEach ELSE 0 END) AS June,
+    SUM(CASE WHEN MONTH(o.orderDate) = 7 THEN od.quantityOrdered * od.priceEach ELSE 0 END) AS July,
+    SUM(CASE WHEN MONTH(o.orderDate) = 8 THEN od.quantityOrdered * od.priceEach ELSE 0 END) AS August,
+    SUM(CASE WHEN MONTH(o.orderDate) = 9 THEN od.quantityOrdered * od.priceEach ELSE 0 END) AS September,
+    SUM(CASE WHEN MONTH(o.orderDate) = 10 THEN od.quantityOrdered * od.priceEach ELSE 0 END) AS October,
+    SUM(CASE WHEN MONTH(o.orderDate) = 11 THEN od.quantityOrdered * od.priceEach ELSE 0 END) AS November,
+    SUM(CASE WHEN MONTH(o.orderDate) = 12 THEN od.quantityOrdered * od.priceEach ELSE 0 END) AS December
+FROM customers c
+JOIN orders o ON c.customerNumber = o.customerNumber
+JOIN orderdetails od ON o.orderNumber = od.orderNumber
+JOIN products p ON od.productCode = p.productCode
+WHERE c.customerName = "La Rochelle Gifts"
+
+UNION ALL
+
+SELECT 
+    'Purchase Expense' AS type,
+    SUM(CASE WHEN MONTH(o.orderDate) = 1 THEN od.quantityOrdered * p.buyPrice ELSE 0 END) AS January,
+    SUM(CASE WHEN MONTH(o.orderDate) = 2 THEN od.quantityOrdered * p.buyPrice ELSE 0 END) AS February,
+    SUM(CASE WHEN MONTH(o.orderDate) = 3 THEN od.quantityOrdered * p.buyPrice ELSE 0 END) AS March,
+    SUM(CASE WHEN MONTH(o.orderDate) = 4 THEN od.quantityOrdered * p.buyPrice ELSE 0 END) AS April,
+    SUM(CASE WHEN MONTH(o.orderDate) = 5 THEN od.quantityOrdered * p.buyPrice ELSE 0 END) AS May,
+    SUM(CASE WHEN MONTH(o.orderDate) = 6 THEN od.quantityOrdered * p.buyPrice ELSE 0 END) AS June,
+    SUM(CASE WHEN MONTH(o.orderDate) = 7 THEN od.quantityOrdered * p.buyPrice ELSE 0 END) AS July,
+    SUM(CASE WHEN MONTH(o.orderDate) = 8 THEN od.quantityOrdered * p.buyPrice ELSE 0 END) AS August,
+    SUM(CASE WHEN MONTH(o.orderDate) = 9 THEN od.quantityOrdered * p.buyPrice ELSE 0 END) AS September,
+    SUM(CASE WHEN MONTH(o.orderDate) = 10 THEN od.quantityOrdered * p.buyPrice ELSE 0 END) AS October,
+    SUM(CASE WHEN MONTH(o.orderDate) = 11 THEN od.quantityOrdered * p.buyPrice ELSE 0 END) AS November,
+    SUM(CASE WHEN MONTH(o.orderDate) = 12 THEN od.quantityOrdered * p.buyPrice ELSE 0 END) AS December
+FROM customers c
+JOIN orders o ON c.customerNumber = o.customerNumber
+JOIN orderdetails od ON o.orderNumber = od.orderNumber
+JOIN products p ON od.productCode = p.productCode
+WHERE c.customerName = "La Rochelle Gifts"
+
+UNION ALL
+
+SELECT 
+    'Profit' AS type,
+    SUM(CASE WHEN MONTH(o.orderDate) = 1 THEN (od.quantityOrdered * od.priceEach) - (od.quantityOrdered * p.buyPrice) ELSE 0 END) AS January,
+    SUM(CASE WHEN MONTH(o.orderDate) = 2 THEN (oxd.quantityOrdered * od.priceEach) - (od.quantityOrdered * p.buyPrice) ELSE 0 END) AS February,
+    SUM(CASE WHEN MONTH(o.orderDate) = 3 THEN (od.quantityOrdered * od.priceEach) - (od.quantityOrdered * p.buyPrice) ELSE 0 END) AS March,
+    SUM(CASE WHEN MONTH(o.orderDate) = 4 THEN (od.quantityOrdered * od.priceEach) - (od.quantityOrdered * p.buyPrice) ELSE 0 END) AS April,
+    SUM(CASE WHEN MONTH(o.orderDate) = 5 THEN (od.quantityOrdered * od.priceEach) - (od.quantityOrdered * p.buyPrice) ELSE 0 END) AS May,
+    SUM(CASE WHEN MONTH(o.orderDate) = 6 THEN (od.quantityOrdered * od.priceEach) - (od.quantityOrdered * p.buyPrice) ELSE 0 END) AS June,
+    SUM(CASE WHEN MONTH(o.orderDate) = 7 THEN (od.quantityOrdered * od.priceEach) - (od.quantityOrdered * p.buyPrice) ELSE 0 END) AS July,
+    SUM(CASE WHEN MONTH(o.orderDate) = 8 THEN (od.quantityOrdered * od.priceEach) - (od.quantityOrdered * p.buyPrice) ELSE 0 END) AS August,
+    SUM(CASE WHEN MONTH(o.orderDate) = 9 THEN (od.quantityOrdered * od.priceEach) - (od.quantityOrdered * p.buyPrice) ELSE 0 END) AS September,
+    SUM(CASE WHEN MONTH(o.orderDate) = 10 THEN (od.quantityOrdered * od.priceEach) - (od.quantityOrdered * p.buyPrice) ELSE 0 END) AS October,
+    SUM(CASE WHEN MONTH(o.orderDate) = 11 THEN (od.quantityOrdered * od.priceEach) - (od.quantityOrdered * p.buyPrice) ELSE 0 END) AS November,
+    SUM(CASE WHEN MONTH(o.orderDate) = 12 THEN (od.quantityOrdered * od.priceEach) - (od.quantityOrdered * p.buyPrice) ELSE 0 END) AS December
+FROM customers c
+JOIN orders o ON c.customerNumber = o.customerNumber
+JOIN orderdetails od ON o.orderNumber = od.orderNumber
+JOIN products p ON od.productCode = p.productCode
+WHERE c.customerName = "La Rochelle Gifts";
+
+
+
 
 
 
